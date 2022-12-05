39a6478cbed1667ccef10bd43a179407c081e71a
Deias <saied_am@hotmail.com>
2022-12-05 16:17:05 +0100

    setting up the config file for different environments: production, development, testing.

---
+++ config.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+class Config(object):
+    SECRET_KEY = os.environ["SECRET_KEY"]
+    # Grabs the folder where the script runs.
+    BASEDIR = os.path.abspath(os.path.dirname(__file__))
+    TESTING = False
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = (
+        f"sqlite:///{os.path.join(Config.BASEDIR, 'data', 'prod_db.sqlite3')}"
+    )
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = (
+        f"sqlite:///{os.path.join(Config.BASEDIR, 'data', 'dev_db.sqlite3')}"
+    )
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
