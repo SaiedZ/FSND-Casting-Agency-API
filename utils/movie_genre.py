c63c080f732c9825b21ef129a1e0e4b3887b5c9f
Deias <saied_am@hotmail.com>
2022-12-06 17:11:47 +0100

    helper class for movies genre, usage of Enum class

---
+++ utils/movie_genre.py
@@ -0,0 +1,50 @@
+"""
+Movie Genre Enum.
+"""
+
+
+import enum
+
+
+class MovieGenreEnum(enum.Enum):
+
+    Action = "Action",
+    Adventure = "Adventure",
+    Animated = "Animated",
+    Biography = "Biography",
+    Comedy = "Comedy",
+    Crime = "Crime",
+    Dance = "Dance",
+    Disaster = "Disaster",
+    Documentary = "Documentary",
+    Drama = "Drama",
+    Erotic = "Erotic",
+    Family = "Family",
+    Fantasy = "Fantasy",
+    FoundFootage = "Found Footage",
+    Historical = "Historical",
+    Horror = "Horror",
+    Independent = "Independent",
+    Legal = "Legal",
+    LiveAction = "Live Action",
+    MartialArts = "Martial Arts",
+    Musical = "Musical",
+    Mystery = "Mystery",
+    Noir = "Noir",
+    Performance = "Performance",
+    Political = "Political",
+    Romance = "Romance",
+    Satire = "Satire",
+    ScienceFiction = "Science Fiction",
+    Short = "Short",
+    Silent = "Silent",
+    Slasher = "Slasher",
+    Sports = "Sports",
+    Spy = "Spy",
+    Superhero = "Superhero",
+    Supernatural = "Supernatural",
+    Suspense = "Suspense",
+    Teen = "Teen",
+    Thriller = "Thriller",
+    War = "War",
+    Western = "Western",
