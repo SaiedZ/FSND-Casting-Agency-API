be699b451305e710caf0ad14217a51e86c4d23e6
Deias <saied_am@hotmail.com>
2022-12-06 15:22:14 +0100

    adding docstring and blueprints to the app.py

--- app.py
+++ app.py
@@ -1,27 +1,53 @@
+"""
+This is the main module of the application.
+
+It contains the app factory, applies config, sets the database and the CORS.s
+"""
+
 import os
-from flask import Flask, request, abort, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 from flask_cors import CORS
 
 from config import ProductionConfig, DevelopmentConfig, TestingConfig
 
-from data.models import Movie, Actor
+from data.models import Movie, Actor  # noqa
 from data.db import db_setup
 
+from utils import error_handlers_blueprint
+from routes import actors_blueprint, movies_blueprint
 
-def create_app(test_config=None):
 
-    # create and configure the app
+def create_app(test_config=False):
+    """Creates and configure the app
+
+    params:
+        test_config:boolean - if set to True, the TestingConfig will be applied
+    returns:
+        the flask app
+    """
+
+    # Creating the Flask app
     app = Flask(__name__)
 
+    # Applying the config
     if os.environ.get('IS_DEPLOYED', False):
         app.config.from_object(ProductionConfig)
-    elif test_config is not None:
-        app.config.from_object(test_config)
+
+    elif test_config:
+        app.config.from_object(TestingConfig)
+
     else:
         app.config.from_object(DevelopmentConfig)
 
+    # Setting Cors
     CORS(app)
+
+    # Register blueprints
+    app.register_blueprint(error_handlers_blueprint)
+    app.register_blueprint(actors_blueprint)
+    app.register_blueprint(movies_blueprint)
+
+    # Setting up the database
     db = db_setup(app)
 
     with app.app_context():
