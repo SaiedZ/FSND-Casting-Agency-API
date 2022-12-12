91aca5e4cd6735fad448fb4a4b1806cfff17a451
Deias <saied_am@hotmail.com>
2022-12-12 17:22:55 +0100

    removing some sensitive data from setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,1 @@
 export FLASK_APP=flaskr
-export AUTH0_CLIENT_ID="F5K8tpa7bhxahtOJPh55kx3vLYiAm9yf"
-export AUTH0_CLIENT_SECRET="d_eMXQvcAcpDDiCh7tfbb_6fe9Zdp0jOBjwWZ9TV4LMP36JEPq1rGErnT6PwKaBb"
-export AUTH0_DOMAIN='saied-zouaoui.eu.auth0.com'
-export ALGORITHMS=['RS256']
-export API_AUDIENCE='castingagencyapi'
